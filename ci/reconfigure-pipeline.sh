05ac60701c2133891d7a48a08595a0380a95108c
CF Garden <garden@pivotal.io>
2017-06-30 14:20:31 +0100

    pipeline wip scripts

---
+++ ci/reconfigure-pipeline.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -ex
+cd $(dirname $0)/..
+
+flyrc_target=$1
+if [ -z $flyrc_target ]; then
+  echo "No target passed, using 'grootfs-ci'"
+  flyrc_target="grootfs-ci"
+fi
+[ -z $DEBUG ] && DEBUG=0
+
+main() {
+  check_fly_alias_exists
+  sync_and_login
+
+  pipeline_name="grootfs-diagnostics"
+  vars_name="gcp"
+  pipeline_file="pipeline.yml"
+
+  if [ $DEBUG -eq 1 ]; then
+    pipeline_name="grootfs-test"
+    vars_name="dummy"
+
+    spruce --concourse merge ci/$pipeline_file ci/dummy-pipeline-spruce.yml > ci/dummy_pipeline.yml
+    pipeline_file="dummy_pipeline.yml"
+  fi
+
+  set_pipeline
+  expose_pipeline
+}
+
+set_pipeline() {
+  fly --target="$flyrc_target" set-pipeline --pipeline=$pipeline_name \
+    --config=ci/${pipeline_file} --load-vars-from=$HOME/workspace/grootfs-ci-secrets/vars/$vars_name.yml \
+    --var gnome-private-key="$(lpass show 'Shared-Garden/grootfs-deployments/github-garden-gnome' --notes)" \
+    --var github-access-token="$(lpass show 'Shared-Garden/Garden-Gnome-Github-Account' --field=api-key)" \
+    --var gamora-bosh-username="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/bosh-director' --username)" \
+    --var gamora-bosh-password="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/bosh-director' --password)" \
+    --var gamora-bosh-certificates="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/certificates' --notes)" \
+    --var dockerhub-username="$(lpass show 'Shared-Garden/cf-garden-docker' --username)" \
+    --var dockerhub-password="$(lpass show 'Shared-Garden/cf-garden-docker' --password)" \
+    --var garden-tracker-token="$(lpass show 'Shared-Garden/Garden-Gnome-Tracker-Account' --notes)" \
+    --var aws-access-key-id="$(lpass show "Shared-Garden/grootfs-deployments\thanos/aws-keys" --username)" \
+    --var aws-secret-access-key="$(lpass show "Shared-Garden/grootfs-deployments\thanos/aws-keys" --password)" \
+    --var datadog-api-key="$(lpass show 'Shared-Garden/grootfs-deployments/datadog-api-keys' --username)" \
+    --var datadog-application-key="$(lpass show 'Shared-Garden/grootfs-deployments/datadog-api-keys' --password)" \
+    --var cf-username="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-creds' --username)" \
+    --var cf-password="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-creds' --password)" \
+    --var cf-secrets="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-secrets' --notes)" \
+    --var cf-uaa-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-uaa-certs' --notes)" \
+    --var cf-etcd-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-etcd-certs' --notes)" \
+    --var cf-cc-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-cc-certs' --notes)" \
+    --var cf-consul-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-consul-certs' --notes)" \
+    --var cf-diego-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-diego-certs' --notes)" \
+    --var cf-loggregator-certs="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-loggregator-certs' --notes)" \
+    --var cf-networking="$(lpass show 'Shared-Garden/grootfs-deployments\gamora/cf-networking' --notes)" \
+    --var github-client-id="$(lpass show 'Shared-Garden/grootfs-deployments/github-garden-gnome' --username)" \
+    --var github-client-secret="$(lpass show 'Shared-Garden/grootfs-deployments/github-garden-gnome' --password)" \
+    --var grootfs-release-private-yaml="$(lpass show 'Shared-Garden/grootfs-release-private.yml' --notes)" \
+    \
+    --var thanos-bosh-username="$(lpass show 'Shared-Garden/grootfs-deployments\thanos/bosh-director' --username)" \
+    --var thanos-bosh-password="$(lpass show 'Shared-Garden/grootfs-deployments\thanos/bosh-director' --password)" \
+    --var thanos-bosh-certificates="$(lpass show 'Shared-Garden/grootfs-deployments\thanos/certificates' --notes)" \
+    --var thanos-cf-diego-certs="$(lpass show 'Shared-Garden/grootfs-deployments\thanos/cf-diego-certs' --notes)"
+}
+
+expose_pipeline() {
+  fly --target="$flyrc_target" expose-pipeline --pipeline="$pipeline_name"
+}
+
+check_fly_alias_exists() {
+  set +e
+  grep $flyrc_target ~/.flyrc > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "Please ensure $flyrc_target exists in ~/.flyrc and that you have run fly login"
+    exit 1
+  fi
+  set -e
+}
+
+sync_and_login() {
+  fly -t $flyrc_target sync
+
+  set +e
+  fly -t $flyrc_target containers > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    fly -t $flyrc_target login
+  fi
+  set -e
+}
+
+main
---
+++ ci/scripts/collect-data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+event_text=$(cat dstate-event/event.json | jq .text)
+dstate_json=$(echo "$event_test" | tr -d '\')
+
+CELL_NAME=$(echo $dstate_json | jq '.cell_name')
+CELL_ID=$(echo $dstate_json | jq '.cell_id')
+TAR_PATH=$(echo $dstate_json | jq '.logs_path')
+
+echo "$BOSH_CERTIFICATES" > certificates.yml
+bosh2 int --path "/certs/ca_cert" certificates.yml > ca_cert.crt
+
+echo "Running performance tests..."
+bosh2 -e $BOSH_TARGET --ca-cert ca_cert.crt alias-env bosh-director
+bosh2 -e bosh-director --client $BOSH_CLIENT --client-secret $BOSH_CLIENT_SECRET login
+
+bosh2 -e bosh-director -d $BOSH_DEPLOYMENT scp $CELL_NAME/$CELL_ID:$TAR_PATH diagnostics
