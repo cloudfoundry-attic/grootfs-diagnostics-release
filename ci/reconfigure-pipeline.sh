21f3fbde31e133ed2c40b4ae4429bf5b14dbd799
Tiago Scolari <tscolari@pivotal.io>
2017-07-04 16:27:29 +0100

    Fixes grootfs-diagnostics pipeline on lakitu
    * Correct BOSH auth
    * Run job on prod concourse worker
    * Correct datadog app key
    
    [finishes #148221143]
    
    Signed-off-by: Gareth Clay <gclay@pivotal.io>

--- ci/reconfigure-pipeline.sh
+++ ci/reconfigure-pipeline.sh
@@ -31,7 +31,7 @@ set_pipeline_lakitu() {
     --var slack-alert-url="$(lpass show 'Shared-Garden/grootfs-pivotal-slack-hook' --url)" \
     --var aws-access-key-id="$(lpass show "Shared-Garden/grootfs-deployments/grootfs-dstate-reports-s3-user" --username)" \
     --var aws-secret-access-key="$(lpass show "Shared-Garden/grootfs-deployments/grootfs-dstate-reports-s3-user" --password)" \
-    --var datadog-application-key="$(lpass show 'Shared-Garden/grootfs-deployments/datadog-api-keys' --password)" \
+    --var datadog-application-key="$(lpass show 'Shared-Garden/grootfs-deployments/pws-datadog-app-key' --password)" \
     --var bosh-deployment-name=cf-cfapps-io2-diego \
     --load-vars-from $HOME/workspace/secrets-prod/ci/ci_bosh_secrets.yml \
     --load-vars-from $HOME/workspace/secrets-prod/ci/ci_app_specific_configs.yml
